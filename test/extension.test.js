9c44c455ef6f5ea22054e03dda44987787411351
Marek Lewandowski <marek@mlewandowski.com>
2016-12-07 22:31:01 +0100

    Keep quoting style consistent.

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,6 +1,6 @@
 /* global suite, test */
 
-// 
+//
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
@@ -14,10 +14,10 @@ var vscode = require('vscode');
 var myExtension = require('../extension');
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function() {
+suite('Extension Tests', function() {
 
 	// Defines a Mocha unit test
-	test("Something 1", function() {
+	test('Something 1', function() {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
 		assert.equal(-1, [1, 2, 3].indexOf(0));
 	});
